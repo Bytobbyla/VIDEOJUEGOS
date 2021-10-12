f1b4031573202422b7d5f0b3587ac27feaec7c01
TheSoullessHuman <catalinaoficial@gmail.com>
2021-10-12 00:08:50 -0500

    Dialogo Beta
    El dialogo ya funciona en la primera escena, falta colocar el npc como trigger en vez del botón cambiando el on click por un botón

--- Assets/script/Dialogos/DialogueManager.cs
+++ Assets/script/Dialogos/DialogueManager.cs
@@ -8,6 +8,7 @@ public class DialogueManager : MonoBehaviour
 {
     public Text nameText;
     public Text dialogueText;
+    public Animator animator;
 
     public Queue<string> sentences;
     // Start is called before the first frame update
@@ -17,35 +18,46 @@ public class DialogueManager : MonoBehaviour
 
     }
 
-    public void StartDialogo (Dialogo dialogo) 
+    public void StartDialogo(Dialogo dialogo)
     {
-        nameText.text = dialogueText.name;
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogo.name;
 
         sentences.Clear();
-        foreach (string sentence in dialogo.sentences) 
+        foreach (string sentence in dialogo.sentences)
         {
             sentences.Enqueue(sentence);
         }
         DisplayNextSentence();
     }
 
-    public void DisplayNextSentence ()
+    public void DisplayNextSentence()
     {
-        if (sentences.Count == 0) 
+        if (sentences.Count == 0)
         {
             EndDialogo();
             return;
         }
 
         string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
     }
-    void EndDialogo() 
+
+    void EndDialogo()
     {
         Debug.Log("fin de la conversacion");
-    
-    } 
+        animator.SetBool("IsOpen", false);
 
+    }
 
-
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
 }
